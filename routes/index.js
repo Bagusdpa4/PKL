a97a854aaec3605740419a2a8820baddd458d795
Bagus Dwi <sugab.dwi88@gmail.com>
2024-06-23 21:35:43 +0700

    First Init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yaml");
+const fs = require("fs");
+const path = require("path");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const swagger_path = path.resolve(__dirname, "../docs/api-docs.yaml");
+const file = fs.readFileSync(swagger_path, "utf-8");
+
+// API Docs
+const swaggerDocument = YAML.parse(file);
+router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 
 module.exports = router;
