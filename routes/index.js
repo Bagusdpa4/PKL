f71b575c79653a1af69572c6adb58c4dd11d3175
Dewangga <hamadakinose@gmail.com>
2024-06-25 16:39:13 +0700

    Destination API (Countries Endpoint) Added

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const User = require("./user.routes")
 const Profile = require("./profile.routes")
 const cityRoute = require("./city.routes")
 const Flights = require("./flight.routes")
+const Country = require("./country.routes")
 const Plane = require("./plane.routes")
 
 const swagger_path = path.resolve(__dirname, "../docs/api-docs.yaml");
@@ -30,6 +31,7 @@ router.use("/api/v1", User)
 router.use("/api/v1", Profile)
 router.use("/api/v1", cityRoute)
 router.use("/api/v1", Flights)
+router.use("/api/v1", Country)
 router.use("/api/v1", Plane)
 
 module.exports = router;
